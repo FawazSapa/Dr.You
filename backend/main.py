f3e9906856a9653b3921b0114e0469483a27fb90
fawazPhc <fawaz.dev@projecthumancity.com>
2024-11-02 22:33:01 -0400

    port command addded

--- backend/main.py
+++ backend/main.py
@@ -293,5 +293,8 @@ def predict():
 
 
 # Run the Flask app
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use PORT environment variable if available
+    app.run(host="0.0.0.0", port=port)
